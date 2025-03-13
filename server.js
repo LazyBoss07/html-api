8d751d702058e33bd5c8e5bb3acd6916ddfcc225
Kishore M A <kishore-pt7904@kishore-pt7904.csez.zohocorpin.com>
2025-03-13 12:05:32 +0530

    Major Patch- HTML Response

--- server.js
+++ server.js
@@ -1,134 +1,164 @@
-const express = require('express');
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const path = require('path');
-const os = require('os');
+const express = require("express");
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Index' });
+app.get("/", (req, res) => {
+  res.json({ message: "Index" });
 });
+
+let browser;
+
+const initializeBrowser = async () => {
+  browser = await puppeteer.launch({
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    // executablePath:"/usr/bin/google-chrome",
+    defaultViewport: { height: 1920, width: 1920 },
+  });
+};
+
+initializeBrowser();
 
 const creepjs = async function (page) {
   console.log("Waiting for Selector...");
   await new Promise((r) => setTimeout(r, 10000)); // 10-second delay
-  await page.waitForSelector('div#fingerprint-data');
-  console.log("Selected");
+  await page.waitForSelector("div#fingerprint-data");
+  console.log("Selector found");
   return await page.evaluate(() => document.documentElement.outerHTML);
 };
 
-const gethtml = async function(url) {
-  const browser = await puppeteer.launch({
-    headless: true, 
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],  
-    // executablePath:"/usr/bin/google-chrome",
-    defaultViewport:{height:1920,width:1920}
-  });
-  const page = await browser.newPage();
-  console.log("Got the URL");
+const fp = async function (page) {
+  const formattedData = {};
+  const context = page.browser().defaultBrowserContext();
 
-  try {
-    let htmlContent=null;
-    await page.goto(url,{ waitUntil: 'networkidle0',timeout:60000});
-    if(url.includes('creepjs')) { 
-    htmlContent = await creepjs(page);
-    }else{
-    await new Promise((r) => setTimeout(r, 10000));
-    htmlContent = await page.content();
-    }
-    await browser.close();
-    // console.log(htmlContent);
-    return htmlContent;
-  } catch (err) {
-    console.error("Error navigating to the URL:", err);
-    await browser.close();
-    throw new Error('Error fetching HTML content');
-  }
-};
+  await context.overridePermissions("https://demo.fingerprint.com/playground", [
+    "clipboard-read",
+    "clipboard-write",
+    "clipboard-sanitized-write",
+  ]);
 
-const saveHtmlToFile = async (htmlContent) => {
-  console.log("File Handling");
-  const systemName = os.hostname(); // Get system hostname
-  const timestamp = new Date().toISOString().replace(/[:.]/g, "-"); // Format timestamp
-  const filename = `${systemName}_${timestamp}.html`; // Construct filename
-  const filePath = path.join(__dirname, filename);
-  console.log("Writing File");
-  fs.writeFileSync(filePath, htmlContent);
-  return filePath;
-};
+  await page.bringToFront();
+  await page.waitForSelector('button[aria-label="Copy to clipboard"]',{waitUntil: 'networkidle0'});
 
-app.get('/html', async (req, res) => {
-  try {
-    const url = req.query.url;
-    if (!url) {
-      return res.status(400).json({ error: 'URL query parameter is required' });
-    }
+  const copyButtons = await page.$$('button[aria-label="Copy to clipboard"]');
+  const title = await page.$$("h4");
 
-    console.log(`Fetching HTML from: ${url}`);
-    const htmlContent = await gethtml(url);
-    console.log("Creating File");
-    const filePath = await saveHtmlToFile(htmlContent);
-    
-    res.sendFile(filePath, (err) => {
-      if (err) {
-        console.error("Error sending file:", err);
-        res.status(500).json({ error: "Failed to send file" });
-      } else {
-        console.log(`File sent successfully: ${filePath}`);
+  await new Promise((r) => setTimeout(r, 1000));
 
-        // Delete the file after sending
-        fs.unlink(filePath, (unlinkErr) => {
-          if (unlinkErr) {
-            console.error("Error deleting file:", unlinkErr);
-          } else {
-            console.log(`File deleted: ${filePath}`);
-          }
-        });
+  function convertObjectsToStrings(obj) {
+    for (const key in obj) {
+      if (typeof obj[key] === "object" && obj[key] !== null) {
+        obj[key] = JSON.stringify(obj[key]);
       }
-    });
-
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Error fetching HTML content' });
+    }
   }
+
+  for (let i = 0; i < copyButtons.length; i++) {
+    let button = copyButtons[i];
+
+    await new Promise((r) => setTimeout(r, 100));
+
+    try {
+      await button.click();
+      console.log("Button clicked");
+    } catch (error) {
+      console.log("Failed to click button:", error);
+    } finally {
+      const clipboardData = await page.evaluate(() =>
+        navigator.clipboard.readText()
+      );
+      const curTitle = await page.evaluate(
+        (el) => el.textContent.trim(),
+        title[i]
+      );
+
+      let parsedData;
+      try {
+        parsedData = JSON.parse(clipboardData);
+        if ("products" in parsedData) {
+          parsedData = parsedData["products"];
+        }
+      } catch (e) {
+        console.error("Failed to parse JSON:", e);
+        parsedData = clipboardData; // Keep as string if parsing fails
+      }
+
+      convertObjectsToStrings(parsedData);
+
+      formattedData[curTitle] = parsedData;
+    }
+  }
+
+  return formattedData;
+};
+
+const processPuppeteer = async function (url) {
+  const page = await browser.newPage();
+  console.log("Navigating to the URL");
+
+  const maxRetries = 3;
+  let attempts = 0;
+  let htmlContent = null;
+
+  while (attempts < maxRetries) {
+    try {
+      let response = await page.goto(url, { waitUntil: "networkidle0", timeout: 60000 });
+
+      // Check if the page navigated to about:blank or if the status code is not 200, and reload if necessary
+      if (page.url() === 'about:blank' || response.status() !== 200) {
+        console.log('Page navigated to about:blank or did not return status 200, reloading...');
+        response = await page.reload({ waitUntil: "networkidle0", timeout: 60000 });
+      }
+
+      if (url.includes("creepjs")) {
+        htmlContent = await creepjs(page);
+      } else if (url.includes("playground")) {
+        htmlContent = await fp(page);
+      } else {
+        await new Promise((r) => setTimeout(r, 10000));
+        htmlContent = await page.content();
+      }
+      await page.close();
+      return htmlContent;
+    } catch (err) {
+      console.error(`Error navigating to the URL (attempt ${attempts + 1}):`, err);
+      attempts++;
+      if (attempts >= maxRetries) {
+        await page.close();
+        throw new Error("Error fetching HTML content after multiple attempts");
+      }
+    }
+  }
+};
+
+
+app.get("/", async (req, res) => {
+  res.json({ message: "Index" });
 });
 
-app.post('/html1', async (req, res) => {
+app.get("/getWS", async (req, res) => {
+  res.json({ WSurl: browser.wsEndpoint() });
+});
+
+app.post("/html", async (req, res) => {
   try {
     const url = req.body.url;
     if (!url) {
-      return res.status(400).json({ error: 'URL is required in request body' });
+      return res.status(400).json({ error: "URL is required in request body" });
     }
 
     console.log(`Fetching HTML from: ${url}`);
-    const htmlContent = await gethtml(url);
-    
-    const filePath = await saveHtmlToFile(htmlContent);
-    res.sendFile(filePath, (err) => {
-      if (err) {
-        console.error("Error sending file:", err);
-        res.status(500).json({ error: "Failed to send file" });
-      } else {
-        console.log(`File sent successfully: ${filePath}`);
-
-        // Delete the file after sending
-        fs.unlink(filePath, (unlinkErr) => {
-          if (unlinkErr) {
-            console.error("Error deleting file:", unlinkErr);
-          } else {
-            console.log(`File deleted: ${filePath}`);
-          }
-        });
-      }
-    });
-
-  }
-// }});}
-   catch (err) {
+    const htmlContent = await processPuppeteer(url);
+    console.log("HTML content fetched successfully");
+    res.json({ htmlContent });
+  } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Error fetching HTML content' });
+    res.status(500).json({ error: "Error fetching HTML content" });
   }
 });
 
